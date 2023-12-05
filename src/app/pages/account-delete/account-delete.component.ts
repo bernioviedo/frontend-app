c4dfa9d29f082a5ec8244834cf20cbf5e217e9f1
Bernardo Oviedo <bernioviedo@yahoo.com.ar>
2023-12-05 17:20:54 -0300

    cambios en accesibilidad

--- src/app/pages/account-delete/account-delete.component.ts
+++ src/app/pages/account-delete/account-delete.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/auth/login.service';
+import { User } from 'src/app/services/auth/user';
+import { RegisterService } from 'src/app/services/auth/register.service';
 
 @Component({
   selector: 'app-account-delete',
   templateUrl: './account-delete.component.html',
   styleUrls: ['./account-delete.component.css']
 })
-export class AccountDeleteComponent {
+export class AccountDeleteComponent implements OnInit{
+  userLoginOn:boolean=false;
+  userData?:User;
 
+  constructor(private loginService:LoginService, private registerService:RegisterService) { }
+
+    ngOnInit(): void {
+      this.loginService.currentUserLoginOn.subscribe(
+        {
+          next:(userLoginOn) => {
+            this.userLoginOn=userLoginOn;
+          }
+        }
+      );
+        this.loginService.currentUserData.subscribe(
+          {
+            next:(userData) => {
+              this.userData=userData;
+            }
+          }
+        )
+}
 }
